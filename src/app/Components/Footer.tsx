5226c9cd9852077bb19090e54362cf779b1f676a
funkee <xxtrm98@protonmail.com>
2023-10-10 16:06:00 -0500

    styled footer preliminary

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import "../Styles/FooterStyles.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-styles">
+      <div className="contact-container">
+        <div className="contact-styles">
+          <h2>Contact Us</h2>
+          <span>123-456-7890</span>
+          <span>info@mysite.com</span>
+        </div>
+        <div className="contact-styles">
+          <h2>Address</h2>
+          <span>500 Terry Francine Street</span>
+          <span>San Francisco, CA 94158</span>
+        </div>{" "}
+        <div className="contact-styles">
+          <h2>Opening Hours</h2>
+          <span>Mon - Fri: 8am - 8pm</span>
+          {/* <span>​Saturday: 9am - 7pm</span>
+          <span>​​Sunday: 9am - 8pm</span> */}
+          <span>Saturday: 9am - 7pm </span>
+          <span>Sunday: 9am - 8pm </span>
+        </div>
+      </div>
+      <div className="copyright">
+        <p>Copyright © 2023</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import Test from "./Components/Test";
 import "../app/Styles/TestStyles.scss";
 import "./Breakpoints/Breakpoints.scss";
 export default function Home() {
-  return (
-    <div className="global-container">
-      <Test />
-    </div>
-  );
+  return <div className="global-container">{<Test />}</div>;
 }
