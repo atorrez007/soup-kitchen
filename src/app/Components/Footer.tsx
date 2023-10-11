e849e8bdef2fe4cae46257fb4d4a5f05cffece07
funkee <xxtrm98@protonmail.com>
2023-10-11 13:41:06 -0500

    recreated footer and styles

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,31 +1,65 @@
 import React from "react";
 import "../Styles/FooterStyles.scss";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="footer-styles">
-      <div className="contact-container">
-        <div className="contact-styles">
-          <h2>Contact Us</h2>
-          <span>123-456-7890</span>
-          <span>info@mysite.com</span>
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="company-container">
+            <h3>Company</h3>
+            <p>Home</p>
+            <p>Menu</p>
+            <p>About</p>
+            <p>Contact Us</p>
+          </div>
         </div>
-        <div className="contact-styles">
-          <h2>Address</h2>
-          <span>500 Terry Francine Street</span>
-          <span>San Francisco, CA 94158</span>
-        </div>{" "}
-        <div className="contact-styles">
-          <h2>Opening Hours</h2>
-          <span>Mon - Fri: 8am - 8pm</span>
-          {/* <span>​Saturday: 9am - 7pm</span>
-          <span>​​Sunday: 9am - 8pm</span> */}
-          <span>Saturday: 9am - 7pm </span>
-          <span>Sunday: 9am - 8pm </span>
+        <div className="middle">
+          <div className="company-container">
+            <h3>Support</h3>
+
+            <p>Help Center</p>
+            <p>Safety Center</p>
+            <p>Community Guidelines</p>
+          </div>
+        </div>
+        <div className="right">
+          <div className="company-container">
+            <h3>Legal</h3>
+            <p>Cookies Policy</p>
+            <p>Privacy Policy</p>
+            <p>Terms of Service</p>
+            <p>Law Enforcement</p>
+          </div>
         </div>
       </div>
       <div className="copyright">
-        <p>Copyright © 2023</p>
+        <p>Copyright © 2023 | A to Z - Tech Solutions</p>
+        <div className="socials">
+          <a target="_blank" href="https://www.linkedin.com/in/atorrez007">
+            <FaLinkedin
+              size={25}
+              style={{ color: "white", marginRight: ".3em" }}
+            />
+          </a>
+        </div>
+        <div className="socials">
+          <a target="_blank" href="https://github.com/atorrez007">
+            <FaGithub
+              size={25}
+              style={{ color: "white", marginRight: ".3em" }}
+            />
+          </a>
+        </div>
+        <div className="socials">
+          <a target="_blank" href="https://twitter.com/atorrez_007">
+            <FaTwitter
+              size={25}
+              style={{ color: "white", marginRight: ".3em" }}
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
